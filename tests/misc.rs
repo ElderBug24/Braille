5c861a34939b8e404d3ce3619e2e27fa613dfeed
R3D <ryuk24.gaming@gmail.com>
2026-04-04 01:14:34 +0200

    set_bit_2d tests

--- tests/misc.rs
+++ tests/misc.rs
@@ -1,4 +1,4 @@
-use braille::{BrailleChar, BrailleCharUnOrdered, byte_to_array, array_to_byte, get_bit, get_bit_2d, set_bit, MASK_ORDERED_TO_UNORDERED, MASK_UNORDERED_TO_ORDERED};
+use braille::{BrailleChar, BrailleCharUnOrdered, byte_to_array, array_to_byte, get_bit, get_bit_2d, set_bit, set_bit_2d, MASK_ORDERED_TO_UNORDERED, MASK_UNORDERED_TO_ORDERED};
 
 
 #[test]
@@ -106,6 +106,40 @@ fn set_bit_() {
 #[should_panic]
 fn set_bit_panic() {
     set_bit(0u8, 8, false);
+}
+
+#[test]
+fn set_bit_2d_() {
+    let mut byte = 0u8;
+
+    byte = set_bit_2d(byte, 0, 0, true);
+    assert_eq!(0b_1000_0000, byte);
+    byte = set_bit_2d(byte, 1, 0, true);
+    assert_eq!(0b_1100_0000, byte);
+    byte = set_bit_2d(byte, 0, 1, true);
+    assert_eq!(0b_1110_0000, byte);
+    byte = set_bit_2d(byte, 1, 1, true);
+    assert_eq!(0b_1111_0000, byte);
+    byte = set_bit_2d(byte, 0, 2, true);
+    assert_eq!(0b_1111_1000, byte);
+    byte = set_bit_2d(byte, 1, 2, true);
+    assert_eq!(0b_1111_1100, byte);
+    byte = set_bit_2d(byte, 0, 3, true);
+    assert_eq!(0b_1111_1110, byte);
+    byte = set_bit_2d(byte, 1, 3, true);
+    assert_eq!(0b_1111_1111, byte);
+}
+
+#[test]
+#[should_panic]
+fn set_bit_2d_panic() {
+    set_bit_2d(0u8, 2, 0, false);
+}
+
+#[test]
+#[should_panic]
+fn set_bit_2d_panic2() {
+    set_bit_2d(0u8, 0, 4, false);
 }
 
 #[test]
